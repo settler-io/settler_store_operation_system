ed8dd5db54309805de8717ec0b7e8cb985c24089
yuma300 <ywakimoto2s@gmail.com>
2026-01-04 18:10:09 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.GITHUB_ACTIONS ? '/settler_store_operation_system' : '',
   images: {
     unoptimized: true,
   },
